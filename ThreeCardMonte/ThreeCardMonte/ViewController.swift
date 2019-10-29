0f6be5ff0968be3aacd9cdf85d21dffc0e16f766
Chelsi Christmas <c.christmas95@gmail.com>
2019-10-29 19:02:36 -0400

    Changed card back to random elements, but there are errors

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -15,14 +15,22 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    
+    let arrayOfCards = ["kingCard", "3Card", "3Card"]
+  
     @IBOutlet weak var card1: UIButton!
     @IBOutlet weak var card2: UIButton!
     @IBOutlet weak var card3: UIButton!
     
-    @IBAction func FlipCard(_ sender: UIButton) {
+    @IBAction func flipCard(_ sender: UIButton) {
         //TODO: Show the other side of the card. So I need it to randomly assign an image to each card (an array of images?) And if the player picks the card(button) that has been assigned the King Card it lets them know they won, else they lost.
-        sender.setBackgroundImage(UIImage(named: "KingCard"), for: .normal)
+        let randomCard = arrayOfCards.randomElement() ?? "3Card"
+        
+        card1.setBackgroundImage(UIImage(named: "\(randomCard)"), for: .normal)
+        card2.setBackgroundImage(UIImage(named: "\(randomCard)"), for: .normal)
+        card3.setBackgroundImage(UIImage(named: "\(randomCard)"), for: .normal)
+
+        
+        // sender.setBackgroundImage(UIImage(named: \(randomCard)), for: .normal)
         
         // Right now all of the cards change to the  same image.
         // TODO: Randomize the the way the image is arranged
