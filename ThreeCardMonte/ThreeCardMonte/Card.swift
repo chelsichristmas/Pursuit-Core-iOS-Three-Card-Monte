193d37f9de0e8af01688f7c2addbc67dd3f50891
Chelsi Christmas <c.christmas95@gmail.com>
2019-10-29 16:29:56 -0400

    Connected Buttons to View Controller

---
+++ ThreeCardMonte/ThreeCardMonte/Card.swift
@@ -0,0 +1,25 @@
+//
+//  Card.swift
+//  ThreeCardMonte
+//
+//  Created by Chelsi Christmas on 10/29/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+struct Card {
+    
+    //properties
+    var deck: [Card]
+    var backOfCard: UIImage
+    
+    // methods
+    // Need to give each button a value for the facen (from the King and 3 cards given)
+    // Need to randomize the value of the cards each time
+    func flipCard() {
+        // I want this method to show what the card loooks like on the front
+    }
+    
+    
+}
--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //buttons
+        
+        
+    }
+    
+    @IBAction func FlipCard(_ sender: UIButton) {
+        
+    }
+    //TODO; Show the other side of the card
+    
+    
 }
-
